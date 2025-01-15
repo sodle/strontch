6c47843da0fccd656bb72a25fe2ece3fb6a483bf
Scott Odle <scott@sjodle.com>
2025-01-14 21:37:28 -0700

    Initial Commit

---
+++ StrontchTests/StrontchTests.swift
@@ -0,0 +1,16 @@
+//
+//  StrontchTests.swift
+//  StrontchTests
+//
+//  Created by Scott Odle on 1/14/25.
+//
+
+import Testing
+
+struct StrontchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
